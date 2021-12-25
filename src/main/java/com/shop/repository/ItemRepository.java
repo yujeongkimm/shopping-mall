b0f9d08b3bf5bbf0110d55e7a9020c3c41468b97
hihi-git <20201111@duksung.ac.kr>
2021-12-25 19:50:13 +0900

    Initial commit

---
+++ src/main/java/com/shop/repository/ItemRepository.java
@@ -0,0 +1,16 @@
+package com.shop.repository;
+
+import com.shop.entity.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface ItemRepository extends JpaRepository<Item, Long> {
+
+    List<Item> findByItemNm(String itemNm);     //상품명으로 데이터 조회
+
+    @Query("select i from Item i where i.itemDetail like %:itemDetail% order by i.price desc")
+    List<Item> findByItemDetail(@Param("itemDetail") String itemDetail);
+}
